073b2bb2b6055622cb7bee058397881f6fd9b87c
Kain Shepard <kainkun100@gmail.com>
2023-09-09 00:33:47 -0700

    Update CharacterAnimator.cs

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using EasyButtons;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 public class CharacterAnimator : MonoBehaviour
 {
