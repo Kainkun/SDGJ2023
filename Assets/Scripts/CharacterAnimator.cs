a5e0d7ca3d08b2eb6d50bb7318b11515ae4fe5a6
Kain Shepard <kainkun100@gmail.com>
2023-09-08 23:44:58 -0700

    Character Stepping cont.

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using EasyButtons;
 using UnityEngine;
 
 public class CharacterAnimator : MonoBehaviour
 {
+    public GameObject sprite;
+
     private float distanceToWaitPosition = 2;
-    private Vector2 waitPosition = new Vector2(-1.5f, -0.8f);
+    private Vector2 waitPosition = new Vector2(-1.5f, 0f);
     private float velocity;
 
     private float stepDistance;
@@ -19,41 +22,53 @@ public class CharacterAnimator : MonoBehaviour
 
     void Start()
     {
+        sprite.SetActive(false);
+
         stepDistance = data.stepCurve.keys[data.stepCurve.length - 1].time;
         stepRemainder = distanceToWaitPosition % stepDistance;
-
-        StartCoroutine(MoveToWaitPosition());
     }
 
-    IEnumerator MoveToWaitPosition()
+    [Button(Mode = ButtonMode.EnabledInPlayMode)]
+    public void MoveToWaitPosition() => StartCoroutine(E_MoveToWaitPosition());
+
+    IEnumerator E_MoveToWaitPosition()
     {
+        sprite.SetActive(true);
+
         Vector2 startPosition = waitPosition + Vector2.left * distanceToWaitPosition;
         transform.position = startPosition;
 
         float t = 0;
-        while (t < distanceToWaitPosition)
+        while (t < distanceToWaitPosition - 0.001f)
         {
+            print(t);
             t = Mathf.SmoothDamp(t, distanceToWaitPosition, ref velocity, data.smoothTime, data.maxSpeed);
             transform.position = startPosition + new Vector2(t, data.stepCurve.Evaluate(t - stepRemainder));
             yield return null;
         }
 
         transform.position = waitPosition;
-
         onWaitPosition?.Invoke();
     }
 
-    IEnumerator MoveToLeave()
+    [Button(Mode = ButtonMode.EnabledInPlayMode)]
+    public void MoveToLeave() => StartCoroutine(E_MoveToLeave());
+
+    IEnumerator E_MoveToLeave()
     {
         Vector2 startPosition = waitPosition;
         transform.position = startPosition;
 
         float t = 0;
-        while (t < distanceToWaitPosition)
+        while (t < distanceToWaitPosition - 0.001f)
         {
+            print(t);
             t = Mathf.SmoothDamp(t, distanceToWaitPosition, ref velocity, data.smoothTime, data.maxSpeed);
             transform.position = startPosition + new Vector2(-t, data.stepCurve.Evaluate(t));
             yield return null;
         }
+
+        sprite.SetActive(false);
+        onDisappear?.Invoke();
     }
 }
